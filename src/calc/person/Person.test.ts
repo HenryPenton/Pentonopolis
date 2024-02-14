a3574cd07ce7cdd5ab8b36cdf777bcaa6bf80a32
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-02-14 20:56:26 +0000

    add data required to calculate total owed

--- src/calc/person/Person.test.ts
+++ src/calc/person/Person.test.ts
@@ -1,15 +1,44 @@
 import { Person } from "./Person";
 describe("Person", () => {
   test("add payment to person history", () => {
+    const payingFor = new Person();
     const person = new Person();
-    person.addPayment(1.23);
-    expect(person.getPaymentHistory()).toEqual([1.23]);
+    person.addPaymentSet(new Set([{ to: payingFor, amount: 1.23 }]));
+    expect(person.getPaymentHistory()).toEqual([
+      new Set([{ to: payingFor, amount: 1.23 }]),
+    ]);
   });
 
   test("add multiple payments to person history", () => {
+    const payingFor1 = new Person();
+    const payingFor2 = new Person();
     const person = new Person();
-    person.addPayment(1.23);
-    person.addPayment(3.21);
-    expect(person.getPaymentHistory()).toEqual([1.23, 3.21]);
+    person.addPaymentSet(
+      new Set([
+        { to: payingFor1, amount: 1.23 },
+        { to: payingFor2, amount: 3.21 },
+      ])
+    );
+
+    expect(person.getPaymentHistory()).toEqual([
+      new Set([
+        { to: payingFor1, amount: 1.23 },
+        { to: payingFor2, amount: 3.21 },
+      ]),
+    ]);
+  });
+
+  test("add negative payments from other people", () => {
+    const person = new Person();
+    const person2 = new Person();
+    const person3 = new Person();
+
+    person.addDebt(person2, 1.23);
+    person.addDebt(person3, 3.21);
+
+    expect(person.getDebts()).toEqual([
+      { by: person2, amount: 1.23 },
+      { by: person3, amount: 3.21 },
+    ]);
   });
 });
--- src/calc/person/Person.ts
+++ src/calc/person/Person.ts
@@ -1,14 +1,27 @@
+type Debt = { by: Person; amount: number };
+type Payment = Set<{ to: Person; amount: number }>;
+
 export class Person {
-  private payments: number[];
+  private payments: Payment[];
+  private debts: Debt[];
 
   constructor() {
     this.payments = [];
+    this.debts = [];
   }
-  addPayment(value: number) {
-    this.payments.push(value);
+  addPaymentSet(payments: Payment) {
+    this.payments.push(payments);
   }
 
   getPaymentHistory() {
     return this.payments;
+  }
+
+  getDebts(): Debt[] {
+    return this.debts;
+  }
+
+  addDebt(person: Person, amount: number) {
+    this.debts.push({ by: person, amount });
   }
 }
