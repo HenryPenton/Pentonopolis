96f0fed4bc45dd8743cc4ae18553df2403568efa
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-02-23 18:29:32 +0000

    move to uuid library

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,3 +1,3 @@
-import { randomUUID } from "crypto";
+import { v4 as uuidv4 } from "uuid";
 
-export const generateNewId = (): string => randomUUID();
+export const generateNewId = (): string => uuidv4();
