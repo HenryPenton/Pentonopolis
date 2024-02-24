9444f7f8c700a85dd4eca19d7e553d88b28d93c4
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-02-24 21:01:41 +0000

    switch to pattern where the controller keeps track of a set of people

--- src/controller/controller.test.ts
+++ src/controller/controller.test.ts
@@ -1,54 +1,95 @@
 import { Person } from "../person/Person";
 import { Controller } from "./controller";
 describe("controller", () => {
+  describe("people", () => {
+    test("the controller gives back a person id when creating a person", () => {
+      const controller = new Controller();
+      const personId = controller.addNewPerson();
+
+      expect(personId).toStrictEqual(expect.any(String));
+    });
+  });
   describe("total spend", () => {
     test("the controller can tell me the total amount that someone has paid (0.00)", () => {
       const controller = new Controller();
-      const person = new Person();
-      person.addPaymentSet(new Set([{ amount: 0, to: new Person() }]));
+      const personId = controller.addNewPerson();
+      const paymentSet = new Set([{ amount: 0, to: new Person() }]);
+      controller.addPaymentSetToPersonById(paymentSet, personId);
 
-      expect(controller.getTotalSpendByPerson(person)).toBe(0.0);
+      expect(controller.getTotalSpendByPersonId(personId)).toBe(0.0);
     });
 
     test("the controller can tell me the total amount that someone has paid (1.57 - single payment) ", () => {
       const controller = new Controller();
-      const person = new Person();
-      person.addPaymentSet(new Set([{ amount: 1.57, to: new Person() }]));
+      const personId = controller.addNewPerson();
+      controller.addPaymentSetToPersonById(
+        new Set([{ amount: 1.57, to: new Person() }]),
+        personId
+      );
 
-      expect(controller.getTotalSpendByPerson(person)).toBe(1.57);
+      expect(controller.getTotalSpendByPersonId(personId)).toBe(1.57);
     });
 
     test("the controller can tell me the total amount that someone has paid (1.28 - single payment, two people) ", () => {
       const controller = new Controller();
-      const person = new Person();
-      person.addPaymentSet(
+      const personId = controller.addNewPerson();
+      controller.addPaymentSetToPersonById(
         new Set([
           { amount: 0.29, to: new Person() },
           { amount: 0.99, to: new Person() },
-        ])
+        ]),
+        personId
       );
 
-      expect(controller.getTotalSpendByPerson(person)).toBe(1.28);
+      expect(controller.getTotalSpendByPersonId(personId)).toBe(1.28);
     });
 
     test("the controller can tell me the total amount that someone has paid (1.28 - two payments, two people) ", () => {
       const controller = new Controller();
-      const person = new Person();
-      person.addPaymentSet(
+      const personId = controller.addNewPerson();
+
+      controller.addPaymentSetToPersonById(
         new Set([
           { amount: 0.29, to: new Person() },
           { amount: 0.99, to: new Person() },
-        ])
+        ]),
+        personId
       );
 
-      person.addPaymentSet(
+      controller.addPaymentSetToPersonById(
         new Set([
           { amount: 0.56, to: new Person() },
           { amount: 0.23, to: new Person() },
-        ])
+        ]),
+        personId
       );
 
-      expect(controller.getTotalSpendByPerson(person)).toBe(2.07);
+      expect(controller.getTotalSpendByPersonId(personId)).toBe(2.07);
+    });
+    describe("errors", () => {
+      test("the controller throws an PersonDoesNotExist error if the person id doesn't relate to a person when creating a payment", () => {
+        const controller = new Controller();
+        controller.addNewPerson();
+        const paymentSet = new Set([{ amount: 0, to: new Person() }]);
+
+        expect(() =>
+          controller.addPaymentSetToPersonById(
+            paymentSet,
+            "some-non-existent-id"
+          )
+        ).toThrow(new Error("That person does not exist"));
+      });
+
+      test("the controller throws an PersonDoesNotExist error if the person id doesn't relate to a person when retrieving a persons total spend", () => {
+        const controller = new Controller();
+        const personId = controller.addNewPerson();
+        const paymentSet = new Set([{ amount: 0, to: new Person() }]);
+        controller.addPaymentSetToPersonById(paymentSet, personId);
+
+        expect(() =>
+          controller.getTotalSpendByPersonId("some-non-existent-id")
+        ).toThrow(new Error("That person does not exist"));
+      });
     });
   });
 
@@ -59,7 +100,7 @@ describe("controller", () => {
       const person = new Person();
       person.addDebt(personToPayBack, 0);
 
-      expect(controller.getTotalSpendByPerson(person)).toBe(0.0);
+      expect(controller.getTotalDebtByPerson(person)).toBe(0.0);
     });
 
     test("the controller can tell me the total amount that someone is in debt (5.27, single debt)", () => {
--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,9 +1,35 @@
-import { Person } from "../person/Person";
+import { PaymentSetup, Person } from "../person/Person";
 
 export class Controller {
-  getTotalSpendByPerson(person: Person): number {
+  private people: Map<string, Person> = new Map();
+
+  addNewPerson(): string {
+    const newPerson = new Person();
+    this.people.set(newPerson.id, newPerson);
+    return newPerson.id;
+  }
+  private getPerson(personId: string): Person {
+    const person = this.people.get(personId);
+
+    if (person) {
+      return person;
+    }
+
+    throw new PersonDoesNotExistError("That person does not exist");
+  }
+
+  addPaymentSetToPersonById(paymentSet: PaymentSetup, personId: string): void {
+    const person = this.getPerson(personId);
+
+    person.addPaymentSet(paymentSet);
+  }
+
+  getTotalSpendByPersonId(personId: string): number {
     let total = 0;
-    person.getPaymentHistory().forEach((paymentSet) => {
+
+    const person = this.getPerson(personId);
+
+    person!.getPaymentHistory().forEach((paymentSet) => {
       paymentSet.payments.forEach((payment) => (total += payment.amount));
     });
 
@@ -17,3 +43,5 @@ export class Controller {
     return Number(totalDebt.toFixed(2));
   };
 }
+
+class PersonDoesNotExistError extends Error {}
