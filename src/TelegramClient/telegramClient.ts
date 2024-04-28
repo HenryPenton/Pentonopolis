0340c1e7c1995ac4bac52c9ac3053c730851742c
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-04-28 10:56:14 +0100

    basic non functional fetch

---
+++ src/TelegramClient/telegramClient.ts
@@ -0,0 +1,19 @@
+export interface IClient {
+  sendMessage: (message: string, chatid: string) => Promise<void>;
+}
+
+export type IFetch = typeof fetch;
+
+export class TelegramClient implements IClient {
+  constructor(private fetch: IFetch) {}
+
+  sendMessage = async (message: string, chatid: string): Promise<void> => {
+    this.fetch("telegram-api-url", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ chat_id: chatid, text: message }),
+    });
+  };
+}
