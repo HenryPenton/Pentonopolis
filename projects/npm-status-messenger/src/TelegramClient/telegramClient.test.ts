6d14f4551255b35058781857d4160a75f69735a6
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-05-06 13:31:12 +0100

    remove dummy test

--- projects/npm-status-messenger/src/TelegramClient/telegramClient.test.ts
+++ projects/npm-status-messenger/src/TelegramClient/telegramClient.test.ts
@@ -13,18 +13,4 @@ describe("Telegram Client", () => {
       body: '{"chat_id":"some-chat-id","text":"some message"}'
     });
   });
-
-  test("dummy test to check nx cache", () => {
-    const stubFetch: IFetch = jest.fn();
-    const telegramClient = new TelegramClient(stubFetch);
-    telegramClient.sendMessage("some message", "some-chat-id");
-
-    expect(stubFetch).toHaveBeenCalledWith("telegram-api-url", {
-      headers: {
-        "Content-Type": "application/json"
-      },
-      method: "POST",
-      body: '{"chat_id":"some-chat-id","text":"some message"}'
-    });
-  });
 });
