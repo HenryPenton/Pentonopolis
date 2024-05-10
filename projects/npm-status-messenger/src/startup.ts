4716911a1884d24c3ca2374d1b9f0fe180eca43e
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-05-10 18:21:25 +0100

    wrap startup in a start function

--- projects/npm-status-messenger/src/startup.ts
+++ projects/npm-status-messenger/src/startup.ts
@@ -10,16 +10,18 @@ const config = new Configuration(
 
 export type IConfig = typeof config;
 
-const telegramClient = new TelegramClient(fetch, config);
+export const run = (): void => {
+  const telegramClient = new TelegramClient(fetch, config);
 
-const dummyReader: IAuditReader = {
-  read: async () => ({
-    metadata: {
-      vulnerabilities: { critical: 1, high: 1, moderate: 1, low: 1, info: 1 }
-    }
-  })
+  const dummyReader: IAuditReader = {
+    read: async () => ({
+      metadata: {
+        vulnerabilities: { critical: 1, high: 1, moderate: 1, low: 1, info: 1 }
+      }
+    })
+  };
+
+  const audit = new NPMAudit(telegramClient, dummyReader);
+
+  audit.fire();
 };
-
-const audit = new NPMAudit(telegramClient, dummyReader);
-
-audit.fire();
