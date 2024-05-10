df35f6cf997cb4c24dce05d7d39f024c9c38bdda
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-05-10 12:18:21 +0100

    rename array

--- shared/array/src/remove/removeFromArray.test.ts
+++ shared/array/src/remove/removeFromArray.test.ts
@@ -1,4 +1,4 @@
-import { removeFromArray } from "./array";
+import { removeFromArray } from "./removeFromArray";
 
 describe("remove from array", () => {
   test("it removes an element from an array of one", () => {
