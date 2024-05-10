f08c27f415d9b4d91d44ee35dfc194a7cad1d93a
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-05-10 16:03:50 +0100

    lint

--- shared/uuid/src/generateUUID/uuid.test.ts
+++ shared/uuid/src/generateUUID/uuid.test.ts
@@ -14,6 +14,7 @@ describe("uuid", () => {
       expect(generateNewId()).toHaveLength(36);
     });
   });
+
   describe("no dashes", () => {
     test("the uuids are strings", () => {
       expect(generateNewId(false)).toEqual(expect.any(String));
