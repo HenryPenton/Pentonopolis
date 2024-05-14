6a730ac1380ee4527bcdddb95c3416e938d81da9
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-05-14 23:47:05 +0100

    implement actual validator

--- projects/npm-status-messenger/src/validator/outdated/NPMOutdatedValidator.test.ts
+++ projects/npm-status-messenger/src/validator/outdated/NPMOutdatedValidator.test.ts
@@ -1,3 +1,59 @@
+import { InvalidDataError } from "../../errors/errors";
+import { OutdatedData } from "../../outdated/outdated";
+import { NpmOutdatedValidator } from "./NPMOutdatedValidator";
 describe("outdated validator", () => {
-  test.todo("implement");
+  test("returns the data if valid for a single dependency", () => {
+    const outdatedValidator = new NpmOutdatedValidator();
+
+    const validData = outdatedValidator.isValid({
+      aaaa: { current: "1.2.3", latest: "2.3.4" }
+    });
+
+    const expectedValidData: OutdatedData = {
+      aaaa: { current: "1.2.3", latest: "2.3.4" }
+    };
+
+    expect(validData).toEqual(expectedValidData);
+  });
+
+  test("returns the data if valid for two dependencies", () => {
+    const outdatedValidator = new NpmOutdatedValidator();
+
+    const validData = outdatedValidator.isValid({
+      aaaa: { current: "1.2.3", latest: "2.3.4" },
+      bbbb: { current: "4.5.6", latest: "5.6.7" }
+    });
+
+    const expectedValidData: OutdatedData = {
+      aaaa: { current: "1.2.3", latest: "2.3.4" },
+      bbbb: { current: "4.5.6", latest: "5.6.7" }
+    };
+
+    expect(validData).toEqual(expectedValidData);
+  });
+
+  test("throws an Invalid Data Error if the object data is not valid", () => {
+    const outdatedValidator = new NpmOutdatedValidator();
+
+    const validationAttempt = (): OutdatedData =>
+      outdatedValidator.isValid({
+        aaaa: { current: "1.2.3", latest: "2.3.4" },
+        aaalflfa: "",
+        bbbb: { current: "4.5.6", latest: "5.6.7" }
+      });
+
+    expect(validationAttempt).toThrow(
+      new InvalidDataError("Invalid outdated data")
+    );
+  });
+  test("throws an Invalid Data Error if the object is actually a string", () => {
+    const outdatedValidator = new NpmOutdatedValidator();
+
+    const validationAttempt = (): OutdatedData =>
+      outdatedValidator.isValid("wrong");
+
+    expect(validationAttempt).toThrow(
+      new InvalidDataError("Invalid outdated data")
+    );
+  });
 });
--- projects/npm-status-messenger/src/validator/outdated/NPMOutdatedValidator.ts
+++ projects/npm-status-messenger/src/validator/outdated/NPMOutdatedValidator.ts
@@ -1,8 +1,19 @@
+import { InvalidDataError } from "../../errors/errors";
 import { OutdatedData } from "../../outdated/outdated";
 import { IValidator } from "../validator";
 
 export class NpmOutdatedValidator implements IValidator<OutdatedData> {
-  isValid = (): OutdatedData => {
-    return {};
+  isValid = (data: unknown): OutdatedData => {
+    const potentialData = data as OutdatedData;
+    const dataMap = new Map(Object.entries(potentialData));
+    const actualOutdatedData: OutdatedData = {};
+    dataMap.forEach((dataPoint, key) => {
+      if (dataPoint.current && dataPoint.latest) {
+        actualOutdatedData[key] = dataPoint;
+      } else {
+        throw new InvalidDataError("Invalid outdated data");
+      }
+    });
+    return actualOutdatedData;
   };
 }
