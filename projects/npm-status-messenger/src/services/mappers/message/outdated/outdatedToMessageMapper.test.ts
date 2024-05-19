85268cf99d96c53ec92e94e6957c365672b75ec6
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-05-19 21:15:47 +0100

    message mapper can take a package name

--- projects/npm-status-messenger/src/services/mappers/message/outdated/outdatedToMessageMapper.test.ts
+++ projects/npm-status-messenger/src/services/mappers/message/outdated/outdatedToMessageMapper.test.ts
@@ -28,4 +28,24 @@ describe("outdatedToMessageMapper", () => {
       "Outdated Packages:\nabc: 4.5.6 -> 5.6.7\ndef: 1.2.3 -> 2.3.4"
     );
   });
+
+  test("includes package name in message", () => {
+    const data: OutdatedData = {
+      abc: {
+        latest: "5.6.7",
+        current: "4.5.6"
+      },
+      def: {
+        latest: "2.3.4",
+        current: "1.2.3"
+      }
+    };
+
+    const packageName = "some-package";
+    const message = mapOutdatedToMessage(data, packageName);
+
+    expect(message).toEqual(
+      "Outdated Packages for some-package:\nabc: 4.5.6 -> 5.6.7\ndef: 1.2.3 -> 2.3.4"
+    );
+  });
 });
--- projects/npm-status-messenger/src/services/mappers/message/outdated/outdatedToMessageMapper.ts
+++ projects/npm-status-messenger/src/services/mappers/message/outdated/outdatedToMessageMapper.ts
@@ -1,8 +1,12 @@
 import { OutdatedData } from "../../../outdated/outdated";
 
-export const mapOutdatedToMessage = (data: OutdatedData): string => {
+export const mapOutdatedToMessage = (
+  data: OutdatedData,
+  packageName?: string
+): string => {
   const outdatedEntriesMap = new Map(Object.entries(data));
-  let message = "Outdated Packages:";
+  let message = "Outdated Packages";
+  message = packageName ? `${message} for ${packageName}:` : `${message}:`;
 
   outdatedEntriesMap.forEach(
     (entry, key) => (message += `\n${key}: ${entry.current} -> ${entry.latest}`)
